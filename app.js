3afe711078a3e3f19e5880613eb664fc9b991e20
Moya0808 <moya0808@gmail.com>
2021-07-18 23:50:33 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,38 +1,21 @@
 // require packages used in the project
 const express = require('express')
-const app = express()
 const exphbs = require('express-handlebars')
-const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
-
 const methodOverride = require('method-override') 
-
-
 // 引用路由器
 const routes = require('./routes')
-
+require('./config/mongoose')
 
 const port = 3000
 
-mongoose.connect('mongodb://localhost/restaurant_list', { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+const app = express()
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
-
 // static setting
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
-
 app.use(methodOverride('_method'))
 
 app.use(routes)
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,20 +1,11 @@
-const mongoose = require('mongoose')
-
 const Restaurant = require('../restaurant')
-
+const db = require('../../config/mongoose')
 const rawData = require('./restaurant.json')
 const restaurantList = rawData.results
 
-mongoose.connect('mongodb://localhost/restaurant_list', { useNewUrlParser: true, useUnifiedTopology: true })
 
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
 // 連線成功
 db.once('open', () => {
-  console.log('mongodb connected!')
   for (const restaurant of restaurantList) {
     Restaurant.create({
       name: restaurant.name,
